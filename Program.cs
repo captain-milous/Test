2c8f8fd98f9c46199dbed1dd513e423e6dcc4a73
captain-milous <milda.tesar3@gmail.com>
2024-06-02 20:35:50 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine(i + ". Hello, World!");
+            }
+        }
+    }
+}
